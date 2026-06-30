f01c3f2c0c442d6b21a0893a34897b1b29d932fe
Tolux001 <toluwalasepeter100@gmail.com>
2026-06-30 19:32:03 +0100

    syntax change

--- Test/firebase.js
+++ Test/firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
-	apiKey: process.env.Next_apiKey,
-	authDomain: process.env.Next_authDomain,
-	projectId: process.env.Next_projectId,
-	storageBucket: process.env.Next_storageBucket,
-	messagingSenderId: process.env.Next_messagingSenderId,
-	appId: process.env.Next_appId,
-	measurementId: process.env.Next_measurementId,
+	apiKey = process.env.Next_apiKey,
+	authDomain = process.env.Next_authDomain,
+	projectId = process.env.Next_projectId,
+	storageBucket = process.env.Next_storageBucket,
+	messagingSenderId = process.env.Next_messagingSenderId,
+	appId = process.env.Next_appId,
+	measurementId = process.env.Next_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
